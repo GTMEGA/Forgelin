e714462b0ca8b5e7d4440f2d794f138e3cc3cf4d
FalsePattern <me@falsepattern.com>
2025-11-03 07:26:17 +0100

    chore: revert changes from upstream commits
    Api and features in our repo are binary compatible with upstream, this commit is for the merge commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,3 @@ plugins {
     id("com.gtnewhorizons.gtnhconvention")
     id("org.jetbrains.kotlin.jvm")
 }
-
-// Shadow source jars too
-val shadowSources = configurations.getByName("shadowSources")
-tasks.sourcesJar {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    from(shadowSources.map { zipTree(it) })
-}
