767e9757e465db46a1941389388a3373c7eeba8e
FalsePattern <me@falsepattern.com>
2025-11-21 07:33:04 +0100

    feat: Add offline jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,20 @@ minecraft_fp {
     }
 }
 
+//Stuff added here will get marked compileOnlyApi, registered in the deploader, and bundled into the offline jar
+val bundledKotlin = listOf(
+    libs.kotlin.stdlib,
+    libs.kotlin.stdlibJdk7,
+    libs.kotlin.stdlibJdk8,
+    libs.kotlin.reflect,
+    libs.jetbrains.annotations,
+    libs.kotlinx.coroutinesCore,
+    libs.kotlinx.coroutinesCoreJvm,
+    libs.kotlinx.coroutinesJdk8,
+    libs.kotlinx.serializationCore,
+    libs.kotlinx.serializationCoreJvm
+)
+
 repositories {
     exclusive(mavenpattern(), "com.falsepattern")
 }
@@ -54,14 +68,36 @@ tasks.shadowJar {
     exclude("it/unimi/dsi/fastutil/**/package-info.class")
 }
 
+val offlineJar = tasks.register<Jar>("offlineJar") {
+    dependsOn(tasks.reobfJar)
+    archiveClassifier = "offline"
+    from(zipTree(tasks.reobfJar.map { it.archiveFile })) {
+        filesMatching("META-INF/kotlindeps.json") {
+            filter {
+                if (it == "    \"https://repo1.maven.org/maven2/\"") {
+                    ""
+                } else {
+                    it
+                }
+            }
+        }
+    }
+    val prefix = "META-INF/falsepatternlib_repo"
+    bundledKotlin.map { it.get() }.forEach { dep ->
+        into("$prefix/${dep.group.replace('.', '/')}/${dep.name}/${dep.version!!}") {
+            from(configurations["compileClasspath"]) {
+                include("${dep.name}-${dep.version}.jar")
+            }
+        }
+    }
+}
+
+tasks.named("assemble") {
+    dependsOn(offlineJar)
+}
+
 dependencies {
-    compileOnlyApi(libs.kotlin.stdlib)
-    compileOnlyApi(libs.kotlin.stdlibJdk8)
-    compileOnlyApi(libs.kotlin.reflect)
-    compileOnlyApi(libs.jetbrains.annotations)
-    compileOnlyApi(libs.kotlinx.coroutinesCore)
-    compileOnlyApi(libs.kotlinx.coroutinesJdk8)
-    compileOnlyApi(libs.kotlinx.serializationCore)
+    bundledKotlin.forEach { compileOnlyApi(it) }
     shadowImplementation(libs.fastutil)
 
     //Deploader
@@ -70,14 +106,27 @@ dependencies {
 }
 
 tasks.processResources {
-    val versions = mapOf(
-        "versionKotlin" to libs.versions.kotlin,
-        "versionAnnotations" to libs.versions.annotations,
-        "versionCoroutines" to libs.versions.coroutines,
-        "versionSerialization" to libs.versions.serialization,
-    )
-    inputs.property("forgelinVersions", versions)
+    val kt = provider{ bundledKotlin }.map { it.map { dep -> dep.get() }.map { dep -> "${dep.group}:${dep.name}:${dep.version!!}" } }
+    inputs.property("bundledKotlin", kt)
     filesMatching("META-INF/kotlindeps.json") {
-        expand(versions.map { (name, ver) -> name to ver.get() }.toMap())
+        expand("kotlinDeps" to run {
+            val res = StringBuilder()
+            kt.get().forEach { dep ->
+                res.append('"').append(dep).append("\",")
+            }
+            res.setLength(res.length - 2)
+            res.delete(0, 1)
+            res.toString()
+        })
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            named<MavenPublication>("maven") {
+                artifact(offlineJar)
+            }
+        }
     }
 }
